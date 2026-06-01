7f471f9261781b71bd63aa129290c29c86d48676
salmansahed <salmansahed10@gmail.com>
2026-06-02 00:11:21 +0600

    Added google login in registration & login page

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -13,4 +13,10 @@ export const auth = betterAuth({
     enabled: true,
     autoSignIn: false,
   },
+  socialProviders: {
+        google: { 
+            clientId: process.env.GOOGLE_CLIENT_ID, 
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET, 
+        }, 
+    },
 });
